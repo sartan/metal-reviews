19700797aa4ff2f6e4e6b239c20f83be1cd26996
Alex Sartan <alex@peskydonut.com>
2018-02-16 21:12:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Metal Reviews
+
+Scrapes CD reviews from [blabbermouth.net](http://www.blabbermouth.net/cdreviews)
---
+++ src/main/kotlin/Reviews.kt
@@ -0,0 +1,23 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.jsoup.Jsoup
+import java.net.URI
+
+fun main(args: Array<String>) {
+    scrape("http://www.blabbermouth.net/cdreviews").map(::toJson).map(::println)
+}
+
+fun scrape(url: String): List<Review> = Jsoup.connect(url).get().run {
+    select("article .entry-content").map {
+        Review(
+          thumb = URI.create(it.selectFirst(".post-thumbnail img").attr("src")),
+          artist = it.selectFirst(".review-meta h2 > a").text(),
+          album = it.selectFirst(".review-meta h2 > a span").text(),
+          rating = it.selectFirst(".review-meta h5").selectFirst("a").text().toDouble()
+        )
+    }
+}.toList()
+
+data class Review(val thumb: URI, val artist: String, val album: String, val rating: Double)
+
+fun toJson(obj: Any): String = ObjectMapper().registerKotlinModule().writeValueAsString(obj)
